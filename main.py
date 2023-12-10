314a5ccdd19e43d58cf738681553dd32ee23efc8
Subhojit Ghosh <subhojit0669@gmail.com>
2023-12-10 16:57:43 +0530

    updated

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello")
+from turtle import Turtle,Screen
+
+screen = Screen()
+screen.title("Ping Pong Game By Subhojit Ghosh")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.exitonclick()
